c8251547893b7fb739e016b4eee88d372f2150b1
sukrutc18 <sukrutmch@gmail.com>
2023-07-26 02:11:53 +0530

    Update

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -41,7 +41,7 @@ const CarCard=({car}:CarCardProps)=>{
             <div className="relative w-full
              h-40 my-3 object-contain">
                 <Image 
-                src="/../public/hero.png"  
+                src="../public/hero.png"  
                 alt="car-model" fill priority 
                 className="object-contain"/>
             </div>
